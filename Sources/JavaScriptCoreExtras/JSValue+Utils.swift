69106a8529cb7e1f0f1ef3736b28c2f8c9b562f5
mhayes853 <matthewha853@gmail.com>
2025-09-28 21:52:55 -0700

    Fix tvOS Build

--- Sources/JavaScriptCoreExtras/JSValue+Utils.swift
+++ Sources/JavaScriptCoreExtras/JSValue+Utils.swift
@@ -1,7 +1,29 @@
 import JavaScriptCore
 
 extension JSValue {
+  /// Whether or not this value is iterable.
   public var isIterable: Bool {
-    self.hasProperty(self.context.evaluateScript("Symbol.iterator"))
+    #if !os(tvOS)
+      self.hasProperty(self.context.evaluateScript("Symbol.iterator"))
+    #else
+      var obj = self.context.objectForKeyedSubscript("_jsCoreExtrasIsIterable")
+      if obj == nil || obj?.isUndefined == true {
+        self.context.installIsIterable()
+        obj = self.context.objectForKeyedSubscript("_jsCoreExtrasIsIterable")
+      }
+      return obj?.call(withArguments: [self]).toBool() ?? false
+    #endif
+  }
+}
+
+extension JSContext {
+  fileprivate func installIsIterable() {
+    self.evaluateScript(
+      """
+      function _jsCoreExtrasIsIterable(obj) {
+        return !!obj[Symbol.iterator];
+      }
+      """
+    )
   }
 }
