a6aa4cc3d96661baf7504f6a1dbab8d549a6b26c
Chris Jones <cmsj@tenshu.net>
2026-09-11 14:33:10 +0100

    Revert "Fix a concurrency issue in JSC-Extras that could cause fetch() continuations to run on the wrong operation queue"
    This reverts commit 453bed7b34f45cb618a97d11bf45604ef39541a2.

--- Sources/JavaScriptCoreExtras/Fetch/JSFetch.swift
+++ Sources/JavaScriptCoreExtras/Fetch/JSFetch.swift
@@ -57,7 +57,7 @@ extension JSContextInstallable where Self == JSFetchInstaller {
       session: URLSession(
         configuration: sessionConfiguration,
         delegate: JSURLSessionDataDelegate(isShared: true),
-        delegateQueue: .current
+        delegateQueue: nil
       )
     )
   }
